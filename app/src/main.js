4d5b4e4389c4023ea6a3d95925446c70b79c7530
TTTTTTTT <TTTTTTTT@example.com>
2025-11-05 01:25:46 +0700

    fix(app): update hello message with extra exclamation marks
    ci(infra): update terraform state with new iam role and policy attachments
    
    feat(ci): implement github actions oidc provider and role for app runner deployment
    
    fix(infra): correct app version and image identifier in app runner configuration
    
    chore(infra): remove unused aws_iam_policy_document data source from terraform state

--- app/src/main.js
+++ app/src/main.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(`Hello from App Runner version NTBX ${version}!\n`);
+  res.end(`Hello!! from App Runner version NTBX ${version}!\n`);
 });
 
 server.listen(port, () => {
